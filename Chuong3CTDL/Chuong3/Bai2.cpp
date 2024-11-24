fdb408d8b689d6be1949e5a5f9782ae3aef9c035
hoangyennewit <144236166+hoangyennewit@users.noreply.github.com>
2024-11-24 23:41:38 +0700

    Update Bai2.cpp

--- Chuong3CTDL/Chuong3/Bai2.cpp
+++ Chuong3CTDL/Chuong3/Bai2.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -242,5 +243,239 @@ int main () {
     L1.remove(L2);
     L1.display();
 
+    return 0;
+}
+*/
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class DSlienket;
+class Node {
+    friend class DSlienket
+;
+private:
+    int data;
+    Node* next;
+public:
+    Node(int _data) {
+        data = _data;
+        next = NULL;
+    }
+};
+
+class DSlienket{
+private:
+    Node* pHead;
+    Node* pTail;
+public:
+    DSlienket
+() {
+        pHead = pTail = NULL;
+    }
+    int Size() {
+        Node* p = pHead;
+        int count = 0;
+        while (p != NULL) {
+            count++;
+            p = p->next;
+        }
+        return count;
+    }
+    void addFirst(int data) {
+        Node* pAdd = new Node(data);
+        if (pHead == NULL) {
+            pHead = pTail = pAdd;
+        }
+        else {
+            pAdd->next = pHead;
+            pHead = pAdd;
+        }
+    }
+    void addLast(int data) {
+        Node* pAdd = new Node(data);
+        if (pHead == NULL) {
+            pHead = pTail = pAdd;
+        }
+        else {
+            pTail->next = pAdd;
+            pTail = pAdd;
+        }
+    }
+    void Remove(int x) {
+        Node* p = pHead, * tmp = NULL;
+        while ((p != NULL) && (p->data != x)) {
+            tmp = p;
+            p = p->next;
+        }
+        if (p == NULL) return;
+        if (tmp != NULL) {
+            if (p == pTail) {
+                pTail = tmp;
+                pTail->next = NULL;
+            }
+            tmp->next = p->next;
+            delete p;
+        }
+        else {
+            pHead = p->next;
+            delete p;
+        }
+    }
+    void SapXep(int flag) {
+        Node* p, * q, * t;
+        for (p = pHead; p->next != NULL; p = p->next) {
+            t = p;
+            for (q = p->next; q != NULL; q = q->next) {
+                if (flag == 1) { // tang dan
+                    if (q->data < t->data) {
+                        t = q;
+                    }
+                }
+                if (flag == -1) { // giam dan
+                    if (q->data > t->data) {
+                        t = q;
+                    }
+                }
+            }
+            swap(t->data, p->data);
+        }
+    }
+    bool search(int x) {
+        Node* p = pHead;
+        while (p != NULL) {
+            if (p->data == x) {
+                return 1;
+            }
+            p = p->next;
+        }
+        return 0;
+    }
+    void D3(DSlienket L1, DSlienket L2) {
+        Node* p1 = L1.pHead;
+        Node* p2 = L2.pHead;
+        while (p1 != NULL) {
+            addLast(p1->data);
+            p1 = p1->next;
+        }
+        while (p2 != NULL) {
+            if (!search(p2->data))
+                addLast(p2->data);
+            p2 = p2->next;
+        }
+    }
+    
+    
+    
+    void D4(DSlienket L1, DSlienket L2) {
+        Node* p1 = L1.pHead;
+        Node* p2 = L2.pHead;
+        while (p1 != NULL) {
+            addLast(p1->data);
+            p1 = p1->next;
+        }
+        
+        while (p2 != NULL) {
+            if (search(p2->data))
+                Remove(p2->data);
+            p2 = p2->next;
+        }
+        int n = Size();
+        Node* p, * q, * t;
+        int i, j;
+        for (p = pHead, i = 0; p->next != NULL, i < (n / 2) - 1; p = p->next, i++) {
+            t = p;
+            for (q = p->next, j = i + 1; q != NULL, j < n / 2; q = q->next, j++) {
+                if (q->data > t->data) {
+                    t = q;
+                }
+            }
+            swap(t->data, p->data);
+        }
+        for (p = p->next, i = n / 2; p->next != NULL, i < n - 1; p = p->next, i++) {
+            t = p;
+            for (q = p->next, j = i + 1; q != NULL, j < n; q = q->next, j++) {
+                if (q->data < t->data) {
+                    t = q;
+                }
+            }
+            swap(t->data, p->data);
+        }
+    }
+    void D5(DSlienket L1, DSlienket L2) {
+        Node* p1 = L1.pHead;
+        Node* p2 = L2.pHead;
+        int n1 = L1.Size();
+        int n2 = L2.Size();
+        int max = n1 > n2 ? n1 : n2;
+        int min = n1 < n2 ? n1 : n2;
+        int i = 0;
+        while (i < min) {
+            addLast(p1->data + p2->data);
+            p1 = p1->next;
+            p2 = p2->next;
+            i++;
+        }
+        while (i < max) {
+            if (p1 != NULL) {
+                addLast(p1->data);
+                p1 = p1->next;
+            }
+            else {
+                addLast(p2->data);
+                p2 = p2->next;
+            }
+            i++;
+        }
+    }
+    void display() {
+        Node* p = pHead;
+        while (p != NULL) {
+            cout << p->data;
+            if(p->next != NULL) cout << " ";
+            p = p->next;
+        }
+    }
+};
+
+void input(DSlienket& DSlienket) {
+    string s;
+    getline(cin, s);
+    char* c = &s[0];
+    while (*c != '\0') {
+        if (isdigit(*c)) {
+            int num = 0;
+            while (isdigit(*c)) {
+                num = num * 10 + *c - 48;
+                c++;
+            }
+            DSlienket
+        .addLast(num);
+        }
+        else {
+            c++;
+        }
+    }
+}
+
+
+
+int main() {
+    DSlienket L1, L2, D3, D4, D5;
+    input(L1);
+    input(L2);
+    D3.D3(L1, L2);
+    D3.SapXep(-1);
+    D3.display();
+    cout << endl;
+    D4.D4(L1, L2);
+    D4.display();
+    cout << endl;
+    D5.D5(L1, L2);
+    D5.display();
+    
     return 0;
 }
